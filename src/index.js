13fc50c7b19e3a76bd0883e6a059a8fe60af2a66
Magesh <magesh@hash14.com>
2018-09-30 22:35:29 +0530

    fetching youtube search data and setting it to state in index js class constructor method

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar"
-
 const API_KEY = "AIzaSyANCvNnjC99rldCY5jP-KPjDdWFvCqPFBY";
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: []}
 
-// Create a new component to produce some html
-
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({ videos: data })
+    });
+  }
+  render(){
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 //Take this components generated HTML and put it on the browser page.
 ReactDOM.render(<App />, document.querySelector('.container'));
