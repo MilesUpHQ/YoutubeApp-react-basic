4db9bc0dd8613c2db96fafce0a2efd16f1811a8a
Magesh <magesh@hash14.com>
2018-09-30 20:37:45 +0530

    created basic component using normal js syntax

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+// Create a new component to produce some html
+
+const App = function () {
+  return <div>Hi!</div>
+}
+//Take this components generated HTML and put it on the browser page.
+ReactDOM.render(<App />, document.querySelector('.container'));
