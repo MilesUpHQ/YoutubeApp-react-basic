c49465d774b21697ecfcd59fa3dbb55ff4474c26
Magesh <magesh@hash14.com>
2018-09-30 21:16:31 +0530

    Added SearchBar component to render input field

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//function component
+const SearchBar = () => {
+  return <input />
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar"
 
+const API_KEY = "AIzaSyANCvNnjC99rldCY5jP-KPjDdWFvCqPFBY";
 // Create a new component to produce some html
 
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 //Take this components generated HTML and put it on the browser page.
 ReactDOM.render(<App />, document.querySelector('.container'));
