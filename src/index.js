2fefc3d16e066372385561c6544ac9f5c4678dec
Magesh <magesh@hash14.com>
2018-09-30 20:40:43 +0530

    updated function to have es6 style definition with fat arrow

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 // Create a new component to produce some html
 
-const App = function () {
+const App = () => {
   return <div>Hi!</div>
 }
 //Take this components generated HTML and put it on the browser page.
